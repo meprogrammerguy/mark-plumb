6123589b907294fb9a280b46f8a91f5f511ed34b
meprogrammerguy <b65535@gmail.com>
2018-05-06 14:54:36 -0500

    scoreboard main page works

--- scoreboard.py
+++ scoreboard.py
@@ -3,13 +3,12 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__, static_folder='static', static_url_path='')
+app = Flask(__name__, static_folder='templates')
 
 @app.route('/')
 def index(name=None):
     return render_template('index.html', name=name)
 
-@app.route('/page')
-@app.route('/<name>/')
-def page(name=None):
-    return render_template('page.html', name=name)
+#@app.route('/page')
+#def page(name=None):
+#    return render_template('page.html', name=name)
