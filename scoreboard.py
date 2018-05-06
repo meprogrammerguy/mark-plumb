b2925f0297b4d04e78a3e5452139e517789d620d
meprogrammerguy <b65535@gmail.com>
2018-05-06 14:14:55 -0500

    scoreboard start

--- scoreboard.py
+++ scoreboard.py
@@ -1,9 +1,15 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
 
-@app.route("/")
-def scoreboard():
-    return "Hello World!"
+app = Flask(__name__, static_folder='static', static_url_path='')
 
+@app.route('/')
+def index(name=None):
+    return render_template('index.html', name=name)
+
+@app.route('/page')
+@app.route('/<name>/')
+def page(name=None):
+    return render_template('page.html', name=name)
