191711abdbeb4399d264fac9c09774622661cba5
meprogrammerguy <b65535@gmail.com>
2018-06-22 11:23:14 -0500

    clean up

--- check_daemon.sh
+++
@@ -1,5 +0,0 @@
-# script to check if daemon is running
-echo "*****************\n"
-echo "folder_daemon PID"
-pgrep -f folder_daemon.py
-echo "\n*****************\n"
--- stop_daemon.sh
+++
@@ -1,2 +0,0 @@
-# script to kill the stock folder daemon
-pkill -9 -f folder_daemon.py
